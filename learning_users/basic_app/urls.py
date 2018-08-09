26181f3e8ba3af90dd2be86562df6abd4a057f07
Manish Singh <sidmanish18@gmail.com>
2018-08-09 16:08:14 +0530

    first

---
+++ learning_users/basic_app/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path ,include
+
+from basic_app import views
+
+app_name = 'basic_app'
+
+urlpatterns = [
+    path('register/', views.register ,name = 'register'),
+    path('login/', views.user_login ,name = 'login'),
+    
+
+]
